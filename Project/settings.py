5d33bbf5abda34d3d2ab689bccebf76888290e14
imbharatchawla <iambharatchawla@gmail.com>
2021-04-06 02:53:16 +0530

    settings

--- Project/settings.py
+++ Project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+3()r_262hrlkpr_hug3*6@h4hv5o4o6g*2i044qnuzi^bwcq3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
