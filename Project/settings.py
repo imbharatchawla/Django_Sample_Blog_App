2a44590ef78e79b50577c935bcf7f066101cfd92
imbharatchawla <iambharatchawla@gmail.com>
2021-04-06 02:49:15 +0530

    added templates, implemented bootstrap in templates, added custom css for html pages, changes in setting for project file, added dynamic routes in html pages

--- Project/settings.py
+++ Project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '+3()r_262hrlkpr_hug3*6@h4hv5o4o6g*2i044qnuzi^bwcq3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogapp.apps.BlogappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# dummy_data
+posts = [
+    {
+        'author': 'John Doe',
+        'date': 'April, 04, 2021',
+        'content_blog': 'This is a blog content',
+        'content_title': 'Blog Heading'
+    },
+    {
+        'author': 'John Doe',
+        'date': 'April, 04, 2021',
+        'content_blog': 'This is a blog content',
+        'content_title': 'Blog Heading 2'
+    }
+]
+
+
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About Blog'})
